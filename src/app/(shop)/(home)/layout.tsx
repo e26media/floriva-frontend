ef5272daa2ddcf75d317d2de3d98340fc3e0b3f3
ismail-is <ismailthalithanooji@gmail.com>
2026-02-26 08:06:47 +0530

    peoduct page updates

--- src/app/(shop)/(home)/layout.tsx
+++ src/app/(shop)/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header/Header'
 import { ApplicationLayout } from '../application-layout'
+import Header2 from '@/components/Header/Header2';
 
 export default function Layout({ children, params }: { children: React.ReactNode; params: any }) {
-  return <ApplicationLayout header={<Header hasBorderBottom={false} />}>{children}</ApplicationLayout>
+  return <ApplicationLayout header={<Header2  />}>{children}</ApplicationLayout>
 }
